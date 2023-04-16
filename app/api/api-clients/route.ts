d013d468b92b460318e215db934f0f2f5287c055
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2023-04-16 18:56:28 +0500

    middleware & registration updated

--- app/api/api-clients/route.ts
+++ app/api/api-clients/route.ts
@@ -7,28 +7,40 @@ export async function POST(request:NextRequest) {
 //    console.log(body);
     const {email,name} =body;
     let query=`SELECT clientName from users WHERE clientEmail in ('${email}');`
+    //console.log(email,name)
 
     const dbc=postgres('postgres://dreamtwister61814:rbnJqy9fciR4@ep-fancy-credit-369457.us-east-2.aws.neon.tech/neondb',{ssl:'prefer'});
 
-    //Check existing users
-
-    const checkEmail=await dbc.unsafe(query);
-    //console.log(checkEmail[0])
-    if(checkEmail[0]){
-        return NextResponse.json({
-            found:'User already exists, please use a different email'
-        })
-    }
-
     //Register new User
 
-    query=`INSERT INTO users(id,clientEmail,clientName,token) 
+    if(email!=undefined && name!=undefined){
+
+        //Check existing users
+
+        const checkEmail=await dbc.unsafe(query);
+        //console.log(checkEmail)
+        //console.log(checkEmail[0])
+        if(checkEmail[0]){
+            return NextResponse.json({
+                found:'User already exists, please use a different email'
+            })
+        }
+
+        query=`INSERT INTO users(id,clientEmail,clientName,token) 
             VALUES(DEFAULT,'${email}','${name}',gen_random_uuid()) RETURNING token;`;
-
-    let result=await dbc.unsafe(query);
-
-    return NextResponse.json({
-        Type:"User created",
-        response:result
-    })
+        let result=await dbc.unsafe(query);
+        return NextResponse.json({
+            Type:"User created",
+            response:result
+        })
+    }else{
+        return NextResponse.json({
+            'Message':"Name or Email cannot be empty"
+        },{
+            status:303,
+            headers:{
+                'content-type':'application/json'
+            }
+        })
+    }
 }
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export const config={
-    matcher:'/api/:novels*',
+    matcher:'/api/protected/:novels*',
 }
 
 
